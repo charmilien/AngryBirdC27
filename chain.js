0310c41dc2bc084cfcdfc1c05dad21c6ce57e7b9
charmilien <charmilien.k@gmail.com>
2020-11-19 17:01:58 +0530

    class for chain added

---
+++ chain.js
@@ -0,0 +1,25 @@
+class Chain
+{
+    constructor(bodyA,bodyB)
+  {
+    var options=
+    {
+        bodyA:bodyA,
+        bodyB:bodyB,
+        stiffness:0.04,
+        length:30
+    }
+    this.chain= Constraint.create(options);
+    World.add(world,this.chain)
+  }
+
+  display()
+  {
+      var posA= this.chain.bodyA.position
+      var posB =this.chain.bodyB.position
+      strokeWeight(4);
+      line(this.chain.bodyA.position.x,this.chain.bodyA.position.y,
+        this.chain.bodyB.position.x,this.chain.bodyB.position.y)
+  }
+}
+
